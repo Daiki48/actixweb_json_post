6890bd0945702654c73ee43cb97cdc8547eafcce
Daiki48 <daiki48.engineer@gmail.com>
2022-08-24 21:22:02 +0900

    [update]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{post, web, App, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Info {
+    str: String,
+    num: i8,
+    arr: Vec::<i8>,
+}
+
+#[post("/postjson")]
+async fn index(info: web::Json<Info>) -> String {
+    format!("str: {} num {} arr: {:?}", info.str, info.num, info.arr)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
